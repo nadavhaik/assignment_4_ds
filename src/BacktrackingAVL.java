7b1bf707a23491283edc65f485838387c447b1b5
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2021-05-10 22:32:54 +0300

    upload to git

---
+++ src/BacktrackingAVL.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class BacktrackingAVL extends AVLTree {
+
+	//You are to implement the function Backtrack.
+    public void Backtrack() {
+        IntegrityStatement.signature(); // Reminder!
+        throw new UnsupportedOperationException("You should implement this");
+    } 
+    
+    //Change the list returned to a list of integers answering the requirements
+    public static List<Integer> AVLTreeBacktrackingCounterExample(){
+        IntegrityStatement.signature(); // Reminder!
+        throw new UnsupportedOperationException("You should implement this");
+    }
+}
---
+++ src/BacktrackingBTree.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
+
+	//You are to implement the function Backtrack.
+	public void Backtrack() {
+	    IntegrityStatement.signature(); // Reminder!
+	    throw new UnsupportedOperationException("You should implement this");
+    }
+	
+	//Change the list returned to a list of integers answering the requirements
+	public static List<Integer> BTreeBacktrackingCounterExample(){
+	    IntegrityStatement.signature(); // Reminder!
+	    throw new UnsupportedOperationException("You should implement this");
+	}
+}

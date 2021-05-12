8c239b280514d80b52974778da0ff59649fabc1f
nivkap <84071374+nivkap@users.noreply.github.com>
2021-05-12 12:18:15 +0300

    the king

--- src/IntegrityStatement.java
+++ src/IntegrityStatement.java
@@ -1,7 +1,7 @@
 
 public final class IntegrityStatement {
     public static String signature() {
-        String names = "Niv Kaplan and Nadav Haik Barouch"; // <- Fill in your names here!
+        String names = "Niv Kaplan and Nadav Haik Barouch the king"; // <- Fill in your names here!
         if (names.length() == 0) {
             throw new UnsupportedOperationException("You should sign here");
         }
